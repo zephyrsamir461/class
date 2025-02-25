a25355191d2ebcfe557a3b2f5e728232c7c460a5
zephyrsamir461 <paudels@sunypoly.edu>
2025-02-25 15:12:32 -0500

    Update classcode.py

--- classcode.py
+++ classcode.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import pandas as pd
 import scipy.stats as stats
 
-st.title(" :bar_chart: Sample Electronic Store EDA")
+
 df=pd.read_csv('online_store_data.csv')
 
 total_sales_per_product = df.groupby('Product')['Total_Sale'].sum().reset_index()
